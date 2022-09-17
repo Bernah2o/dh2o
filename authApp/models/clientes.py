00d469da68b628872718bcacca2b872a99f50908
arley usma <arleyusma@hotmail.com>
2022-09-17 17:07:42 -0500

    creacion clase Clientes

---
+++ authApp/models/clientes.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Clientes(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=100)
+    telefono = models.IntegerField()
+    correo = models.CharField(max_length=100)
+
