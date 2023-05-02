2792e4c982c5d4216c5f668ad5788c1c59413d41
bernardino de aguas <bernardino.deaguas@gmail.com>
2023-05-01 22:25:56 -0500

    botones con css funcionando

--- authApp/admin.py
+++ authApp/admin.py
@@ -4,7 +4,7 @@ from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 from django.urls import reverse
 from django.utils.safestring import mark_safe
-# Register your models here.
+# Importamos los modelos que queremos registrar
 from .models.clientes import Cliente
 from .models.factura import Factura
 from .models.operador import Operador
@@ -15,20 +15,33 @@ from .models.repuesto import Repuesto
 from .models.ordendetrabajo import OrdenDeTrabajo
 from .models.reporte import Reporte
 
-
+# Definimos una clase que hereda de `resources.ModelResource` para especificar la configuración de importación/exportación
 class ClienteResource(resources.ModelResource):
     class Meta:
         model = Cliente
-
+# Definimos una clase que hereda de `ImportExportModelAdmin` y `admin.ModelAdmin` para personalizar el comportamiento del modelo en el sitio de administración
 class ClienteAdmin(ImportExportModelAdmin,admin.ModelAdmin):
+         # Definimos que el campo `creacion` sea de solo lectura
         readonly_fields = ("creacion", )
+         # Especificamos los campos por los que se puede realizar una búsqueda
         search_fields = ['cedula','nombre']
+        # Especificamos los campos que se mostrarán en la lista de objetos del modelo
         list_display = ('cedula','nombre','apellido','direccion','telefono','ver_clientes_proximos')
-        list_display_links = ('cedula',)
+        # Especificamos los campos que tendrán un enlace en la lista de objetos del modelo
+        list_display_links = ('cedula','ver_clientes_proximos')
+         # Especificamos los campos por los que se puede filtrar en la lista de objetos del modelo
         list_filter = ('creacion',)
+        # Especificamos la cantidad de objetos que se mostrarán por página en la lista de objetos del modelo
         list_per_page = 10
+        # Especificamos la configuración de importación/exportación del modelo
         resource_class = ClienteResource
         
+        # Especificamos la hoja de estilos personalizada que queremos utilizar en la página de administración del modelo
+        class Media:
+          css = {
+            'all': ('css/admin.css',)
+        }      
+        # Definimos una función que nos permitirá mostrar un botón verde o rojo dependiendo de si el cliente está próximo a vencerse o no
         def ver_clientes_proximos(self, obj):
             # Obtenemos la fecha actual y la última limpieza del cliente
             fecha_actual = date.today()
@@ -50,10 +63,6 @@ class ClienteAdmin(ImportExportModelAdmin,admin.ModelAdmin):
 
         ver_clientes_proximos.short_description = 'Próx. a vencerse'
         ver_clientes_proximos.allow_tags = True
-class Media:
-        css = {
-            'all': ('css/admin.css',)
-        }      
     
 admin.site.register(Cliente, ClienteAdmin)
 admin.site.register(Factura)
--- authProject/settings.py
+++ authProject/settings.py
@@ -223,7 +223,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'authApp', 'static')
+    os.path.join(BASE_DIR, 'authApp/static')
 ]
 
 
